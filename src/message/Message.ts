00e186f2f9a56b6ffb3b9085140634bca5c42143
Marian Melnychuk <marian.melnychuk@gmail.com>
2025-05-20 23:21:16 +0300

    Fix issue when some properties cannot be parsed in Message object

--- src/message/Message.ts
+++ src/message/Message.ts
@@ -1,21 +1,22 @@
-import { Part } from './Part';
+import { Part } from "./Part";
 
 export class Message {
-    fromfull: string;
-    headers: {};
-    subject: string;
-    parts: Array<Part>;
-    from: string;
-    to: string;
-    id: string;
-    time : number;
-    secondsAgo : number;
-    domain: string;
-    origfrom: string;
-    mrid: string;
-    size : number;
-    stream: string;
-    msgType: string;
-    source: string;
-    text: string;
+  is_first_exchange: boolean;
+  fromfull: string;
+  headers: {};
+  subject: string;
+  parts: Array<Part>;
+  from: string;
+  to: string;
+  id: string;
+  time: number;
+  seconds_ago: number;
+  domain: string;
+  origfrom: string;
+  mrid: string;
+  size: number;
+  stream: string;
+  msg_type: string;
+  source: string;
+  text: string;
 }
