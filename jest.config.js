2ab19753a251348d2deef934d95e4902140b01df
Chris Kenst <ckenst@gmail.com>
2026-03-05 13:21:32 -0800

    Auto-loading .env

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
+    setupFiles: ['dotenv/config'],
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
